9051bbd5b349f22e07803e53350339d257519e3a
NahyunLeylaLee <florence5107@gmail.com>
2025-08-08 02:50:10 -0700

    setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
-export default function Test() {
-  return <h1>Hello NextJs!</h1>;
+export const metadata = {
+  title: "Home",
+};
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Hello!</h1>
+    </div>
+  );
 }
